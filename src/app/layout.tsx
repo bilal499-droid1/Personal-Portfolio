be54a7d14f55621a64a29adae87811ae0e1a8d8c
Muhammad Shaheer Malik <shaheermalik03@gmail.com>
2025-09-21 09:32:26 +0500

    Add favicon files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,31 @@ export const metadata: Metadata = {
     template: `%s | ${DATA.name}`,
   },
   description: DATA.description,
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
   openGraph: {
     title: `${DATA.name}`,
     description: DATA.description,
